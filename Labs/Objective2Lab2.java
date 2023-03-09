e5d7a5ae503fb74e3c0ba9681bf3955ccc61791d
skaddington <skaddington92@gmail.com>
2023-03-09 10:26:32 -0700

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Samantha Addington";
+		int age = 30;
+		String state = "Colorado";
+		System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+	}
+}
