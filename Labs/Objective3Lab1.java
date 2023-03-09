c7dc5792eaa0976a7b41c2c595e5e44244a9f99d
skaddington <skaddington92@gmail.com>
2023-03-09 11:32:03 -0700

    complete objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,10 @@
+public class Objective3Lab1 {
+	public static void main(String[] args) {
+		double num1, num2, num3, average;
+		num1 = 6;
+		num2 = 17;
+		num3 = 11;
+		average = (num1+num2+num3) / 3;
+		System.out.println(average);
+	}
+}
