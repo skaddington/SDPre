723961dcf5f5e6eeee11ca0276ec6549b860ddba
skaddington <skaddington92@gmail.com>
2023-03-09 19:15:08 -0700

    modified switchExample

--- Labs/SwitchExample.java
+++ Labs/SwitchExample.java
@@ -1,22 +1,27 @@
 public class SwitchExample {
 	public static void main(String[] args) {
 		char grade;
-		grade = 'B';
+		grade = 'A';
 
 		switch (grade) {
 		case 'A':
+		case 'a':
 			System.out.println("Fantastic job!");
 			break;
 		case 'B':
+		case 'b':
 			System.out.println("Good job!");
 			break;
 		case 'C':
+		case 'c':
 			System.out.println("You're doing okay.");
 			break;
 		case 'D':
+		case 'd':
 			System.out.println("Please schedule a conference.");
 			break;
 		case 'F':
+		case 'f':
 			System.out.println("Start finishing homework and showing up to class.");
 			break;
 		default:
