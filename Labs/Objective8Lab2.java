e0f3f5ba997e0b50fcc7a1c4d258e5d32f70e663
skaddington <skaddington92@gmail.com>
2023-03-09 21:19:53 -0700

    complete objective8lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,10 @@
+public class Objective8Lab2 {
+	public static void main(String[] args) {
+		int sum = 0;
+
+		for (int i = 1; i <= 20; i +=1){
+			sum = sum + i;
+		}
+		System.out.println(sum);
+	}
+}
